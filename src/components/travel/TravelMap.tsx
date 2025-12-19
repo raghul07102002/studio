895376ce462920a23258bf805c2833dd31aecf96
rajesh2019 raghul <raghulrajesh2019@gmail.com>
2025-12-19 18:33:53 +0000

    "use client";
    import { MapContainer, TileLayer, Polyline, Marker } from

--- src/components/travel/TravelMap.tsx
+++ src/components/travel/TravelMap.tsx
@@ -2,19 +2,19 @@
 
 import { MapContainer, TileLayer, Polyline, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from "leaflet";
 
-// This is to fix the default icon issue with leaflet in React
-const DefaultIcon = L.icon({
-    iconUrl: icon.src,
-    shadowUrl: iconShadow.src,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41]
+// FIX: Leaflet marker icon issue in Next.js
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
-L.Marker.prototype.options.icon = DefaultIcon;
 
 type TravelMapProps = {
   entries: {
