8a779479622f9ec9965d5318080bab7d681470f0
rajesh2019 raghul <raghulrajesh2019@gmail.com>
2025-12-19 09:11:06 +0000

    Try fixing this error: `Runtime Error: useFirebase must be used within a

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,11 +2,17 @@
 'use client';
 import { MainLayout } from "@/components/layout/main-layout";
 import { AppProvider } from "@/contexts/app-provider";
+import { FirebaseClientProvider } from "@/firebase";
+import { AuthProvider } from "@/components/auth/auth-provider";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AppProvider>
-        <MainLayout>{children}</MainLayout>
-    </AppProvider>
+    <FirebaseClientProvider>
+      <AuthProvider>
+        <AppProvider>
+            <MainLayout>{children}</MainLayout>
+        </AppProvider>
+      </AuthProvider>
+    </FirebaseClientProvider>
   );
 }
