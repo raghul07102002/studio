2a357b005a21895acca8bb5fddd7a89d28dc89bb
rajesh2019 raghul <raghulrajesh2019@gmail.com>
2025-12-20 19:37:47 +0000

    use the one i provided

--- src/components/travel/TravelMap.tsx
+++ src/components/travel/TravelMap.tsx
@@ -5,10 +5,13 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 const bikerIcon = new L.Icon({
-    iconUrl: 'https://img.icons8.com/ios-filled/50/person-on-a-bike.png',
-    iconSize: [30, 30],
-    iconAnchor: [15, 30],
-    popupAnchor: [0, -30]
+    iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+    shadowSize: [41, 41]
 });
 
 
